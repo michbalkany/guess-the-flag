9249207db9b9cf60a0a929c59c5e56f479d05cc5
mich balkany <askmitchb@gmail.com>
2023-11-06 11:15:31 -0500

    added animation effects to the flag chosen and flags that were not chosen. animations used were rotation3Deffect, scaleEffect, blur effect and opacity effect.

---
+++ FlagImage.swift
@@ -0,0 +1,20 @@
+//
+//  FlagImage.swift
+//  GuessTheFlag
+//
+//  Created by Mich balkany on 9/20/23.
+//
+
+import SwiftUI
+
+struct FlagImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FlagImage_Previews: PreviewProvider {
+    static var previews: some View {
+        FlagImage()
+    }
+}
