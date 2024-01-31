1395de16c2998fdf40bf5e5475fb963624f0cc4c
mich balkany <askmitchb@gmail.com>
2024-01-31 18:57:13 -0500

    voice over accessability

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -20,7 +20,22 @@ struct ContentView: View {
     @State private var selectedFlag = -1
     
     static let allCountries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    @State private var flagTappedAnimation = false
+    
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+    
+    
     var body: some View {
         ZStack{
             RadialGradient(stops: [
@@ -54,6 +69,7 @@ struct ContentView: View {
                             } label: {
                                 FlagImage(name: countries[number])
                             }
+                            .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                             .rotation3DEffect(
                             .degrees(selectedFlag == number ? 360 : 0), axis: (x: 0, y: 1, z: 0)
                         )
